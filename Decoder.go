34943287de6452c9d423981436c00bc4aa56ec93
Chris Mikkelson <cmikk@fsi.io>
2017-06-19 15:19:17 -0500

    Refactor control frame processing.

--- Decoder.go
+++ Decoder.go
@@ -18,7 +18,6 @@ package framestream
 
 import (
 	"bufio"
-	"bytes"
 	"encoding/binary"
 	"io"
 )
@@ -35,11 +34,6 @@ type Decoder struct {
 	reader  *bufio.Reader
 	writer  *bufio.Writer
 	stopped bool
-}
-
-type ControlFrame struct {
-	ControlType  uint32
-	ContentTypes [][]byte
 }
 
 func NewDecoder(v interface{}, opt *DecoderOptions) (dec *Decoder, err error) {
@@ -60,7 +54,7 @@ func NewDecoder(v interface{}, opt *DecoderOptions) (dec *Decoder, err error) {
 		writer: nil,
 	}
 
-	var cf *ControlFrame
+	var cf ControlFrame
 	if opt.Bidirectional {
 		w, ok := v.(io.Writer)
 		if !ok {
@@ -69,213 +63,36 @@ func NewDecoder(v interface{}, opt *DecoderOptions) (dec *Decoder, err error) {
 		dec.writer = bufio.NewWriter(w)
 
 		// Read the ready control frame.
-		cf, err = dec.readControlFrameType(CONTROL_READY)
+		err = cf.DecodeTypeEscape(dec.reader, CONTROL_READY)
 		if err != nil {
 			return
 		}
 
 		// Check content type.
-		matched := matchContentTypes(cf.ContentTypes, [][]byte{dec.opt.ContentType})
-		if len(matched) != 1 {
+		if !cf.MatchContentType(dec.opt.ContentType) {
 			return dec, ErrContentTypeMismatch
 		}
 
 		// Send the accept control frame.
-		scf := ControlFrame{ControlType: CONTROL_ACCEPT, ContentTypes: matched}
-		err = dec.sendControlFrame(&scf)
+		accept := ControlAccept
+		accept.SetContentType(dec.opt.ContentType)
+		err = accept.EncodeFlush(dec.writer)
 		if err != nil {
 			return
 		}
 	}
 
 	// Read the start control frame.
-	cf, err = dec.readControlFrameType(CONTROL_START)
+	err = cf.DecodeTypeEscape(dec.reader, CONTROL_START)
 	if err != nil {
 		return
 	}
 
 	// Check content type.
-	matched := matchContentTypes(cf.ContentTypes, [][]byte{dec.opt.ContentType})
-	if len(matched) != 1 {
+	if !cf.MatchContentType(dec.opt.ContentType) {
 		return dec, ErrContentTypeMismatch
 	}
 
-	return
-}
-
-func (dec *Decoder) readBE32() (val uint32, err error) {
-	err = binary.Read(dec.reader, binary.BigEndian, &val)
-	if err != nil {
-		return 0, err
-	}
-	return
-}
-
-func (dec *Decoder) readEscape() (err error) {
-	escape, err := dec.readBE32()
-	if err != nil || escape != 0 {
-		dec.stopped = true
-		return
-	}
-	if escape != 0 {
-		err = ErrDecode
-		return
-	}
-	return
-}
-
-func (dec *Decoder) readControlFrame() (cf *ControlFrame, err error) {
-	cf = new(ControlFrame)
-
-	// Read the control frame length.
-	controlFrameLen, err := dec.readBE32()
-	if err != nil {
-		return
-	}
-
-	// Enforce limits on control frame size.
-	if controlFrameLen < 4 || controlFrameLen > MAX_CONTROL_FRAME_SIZE {
-		err = ErrDecode
-		return
-	}
-
-	// Read the control frame.
-	controlFrameData := make([]byte, controlFrameLen)
-	n, err := io.ReadFull(dec.reader, controlFrameData)
-	if err != nil || uint32(n) != controlFrameLen {
-		return
-	}
-
-	// Read the control frame type.
-	p := bytes.NewBuffer(controlFrameData[0:4])
-	err = binary.Read(p, binary.BigEndian, &cf.ControlType)
-	if err != nil {
-		return
-	}
-
-	// Read the control fields.
-	var pos uint32 = 8
-	for pos < controlFrameLen {
-		controlFieldType := binary.BigEndian.Uint32(controlFrameData[pos-4 : pos])
-		switch controlFieldType {
-		case CONTROL_FIELD_CONTENT_TYPE:
-			{
-				pos += 4
-				if pos > controlFrameLen {
-					return cf, ErrDecode
-				}
-				lenContentType := binary.BigEndian.Uint32(controlFrameData[pos-4 : pos])
-				if lenContentType > MAX_CONTROL_FRAME_SIZE {
-					return cf, ErrDecode
-				}
-				pos += lenContentType
-				if pos > controlFrameLen {
-					return cf, ErrDecode
-				}
-				contentType := make([]byte, lenContentType)
-				copy(contentType, controlFrameData[pos-lenContentType:pos])
-				cf.ContentTypes = append(cf.ContentTypes, contentType)
-			}
-		default:
-			return cf, ErrDecode
-		}
-	}
-
-	// Enforce limits on number of ContentType fields.
-	lenContentTypes := len(cf.ContentTypes)
-	switch cf.ControlType {
-	case CONTROL_START:
-		if lenContentTypes > 1 {
-			return cf, ErrDecode
-		}
-	case CONTROL_STOP, CONTROL_FINISH:
-		if lenContentTypes > 0 {
-			return cf, ErrDecode
-		}
-	}
-
-	return
-}
-
-func matchContentTypes(a [][]byte, b [][]byte) (c [][]byte) {
-	matched := make([][]byte, 0, 0)
-	for _, contentTypeA := range a {
-		for _, contentTypeB := range b {
-			if bytes.Compare(contentTypeA, contentTypeB) == 0 {
-				matched = append(matched, contentTypeA)
-			}
-		}
-	}
-	return matched
-}
-
-func (dec *Decoder) sendControlFrame(cf *ControlFrame) (err error) {
-	totalLen := 4 * 3
-	for _, contentType := range cf.ContentTypes {
-		totalLen += 4 + 4 + len(contentType)
-	}
-
-	buf := new(bytes.Buffer)
-
-	// Escape: 32-bit BE integer. Zero.
-	err = binary.Write(buf, binary.BigEndian, uint32(0))
-	if err != nil {
-		return
-	}
-
-	// Frame length: 32-bit BE integer.
-	err = binary.Write(buf, binary.BigEndian, uint32(totalLen-2*4))
-	if err != nil {
-		return
-	}
-
-	// Control type: 32-bit BE integer.
-	err = binary.Write(buf, binary.BigEndian, uint32(cf.ControlType))
-	if err != nil {
-		return
-	}
-
-	for _, contentType := range cf.ContentTypes {
-		// FSTRM_CONTROL_FIELD_CONTENT_TYPE: 32-bit BE integer.
-		err = binary.Write(buf, binary.BigEndian, uint32(CONTROL_FIELD_CONTENT_TYPE))
-		if err != nil {
-			return
-		}
-
-		// Length of content type string: 32-bit BE integer.
-		err = binary.Write(buf, binary.BigEndian, uint32(len(contentType)))
-		if err != nil {
-			return
-		}
-
-		// The content type string itself.
-		_, err = buf.Write(contentType)
-		if err != nil {
-			return
-		}
-	}
-
-	// Write the control frame.
-	_, err = buf.WriteTo(dec.writer)
-	if err != nil {
-		return
-	}
-
-	return dec.writer.Flush()
-}
-
-func (dec *Decoder) readControlFrameType(t uint32) (cf *ControlFrame, err error) {
-	err = dec.readEscape()
-	if err != nil {
-		return
-	}
-	cf, err = dec.readControlFrame()
-	if err != nil {
-		return
-	}
-	if cf.ControlType != t {
-		return cf, ErrDecode
-	}
 	return
 }
 
@@ -301,18 +118,26 @@ func (dec *Decoder) Decode() (frameData []byte, err error) {
 	}
 
 	// Read the frame length.
-	frameLen, err := dec.readBE32()
+	var frameLen uint32
+	err = binary.Read(dec.reader, binary.BigEndian, &frameLen)
 	if err != nil {
 		return
 	}
 	if frameLen == 0 {
 		// This is a control frame.
-		cf, err := dec.readControlFrame()
+		var cf ControlFrame
+		err = cf.Decode(dec.reader)
+		if err != nil {
+			return
+		}
 		if cf.ControlType == CONTROL_STOP {
 			dec.stopped = true
 			if dec.opt.Bidirectional {
 				ff := &ControlFrame{ControlType: CONTROL_FINISH}
-				dec.sendControlFrame(ff)
+				err = ff.EncodeFlush(dec.writer)
+				if err != nil {
+					return
+				}
 			}
 			return nil, EOF
 		}
--- Encoder.go
+++ Encoder.go
@@ -18,7 +18,6 @@ package framestream
 
 import (
 	"bufio"
-	"bytes"
 	"encoding/binary"
 	"io"
 )
@@ -43,7 +42,9 @@ func NewEncoder(w io.Writer, opt *EncoderOptions) (enc *Encoder, err error) {
 	}
 
 	// Write the start control frame.
-	err = enc.writeControlStart()
+	start := ControlStart
+	start.SetContentType(opt.ContentType)
+	err = start.Encode(enc.writer)
 	if err != nil {
 		return
 	}
@@ -52,119 +53,7 @@ func NewEncoder(w io.Writer, opt *EncoderOptions) (enc *Encoder, err error) {
 }
 
 func (enc *Encoder) Close() error {
-	return enc.writeControlStop()
-}
-
-func (enc *Encoder) writeControlStart() (err error) {
-	totalLen := 0
-
-	// Calculate the total amount of space needed for the control frame.
-
-	// Escape: 32-bit BE integer. Zero.
-	totalLen += 4
-
-	// Frame length: 32-bit BE integer.
-	totalLen += 4
-
-	// Control type: 32-bit BE integer.
-	totalLen += 4
-
-	if enc.opt.ContentType != nil {
-		// CONTROL_FIELD_CONTENT_TYPE: 32-bit BE integer.
-		totalLen += 4
-
-		// Length of content type string: 32-bit BE integer.
-		totalLen += 4
-
-		// The content type string itself.
-		totalLen += len(enc.opt.ContentType)
-	}
-
-	// Allocate the storage for the control frame.
-	buf := new(bytes.Buffer)
-
-	// Now actually serialize the control frame.
-
-	// Escape: 32-bit BE integer. Zero.
-	err = binary.Write(buf, binary.BigEndian, uint32(0))
-	if err != nil {
-		return
-	}
-
-	// Frame length: 32-bit BE integer.
-	//
-	// This does not include the length of the escape frame or the length of
-	// the frame length field itself, so subtract 2*4 bytes from the total
-	// length.
-	err = binary.Write(buf, binary.BigEndian, uint32(totalLen-2*4))
-	if err != nil {
-		return
-	}
-
-	// Control type: 32-bit BE integer.
-	err = binary.Write(buf, binary.BigEndian, uint32(CONTROL_START))
-	if err != nil {
-		return
-	}
-
-	if enc.opt.ContentType != nil {
-		// FSTRM_CONTROL_FIELD_CONTENT_TYPE: 32-bit BE integer.
-		err = binary.Write(buf, binary.BigEndian, uint32(CONTROL_FIELD_CONTENT_TYPE))
-		if err != nil {
-			return
-		}
-
-		// Length of content type string: 32-bit BE integer.
-		err = binary.Write(buf, binary.BigEndian, uint32(len(enc.opt.ContentType)))
-		if err != nil {
-			return
-		}
-
-		// The content type string itself.
-		_, err = buf.Write(enc.opt.ContentType)
-		if err != nil {
-			return
-		}
-	}
-
-	// Write the control frame.
-	_, err = buf.WriteTo(enc.writer)
-	if err != nil {
-		return
-	}
-
-	return enc.Flush()
-}
-
-func (enc *Encoder) writeControlStop() (err error) {
-	totalLen := 3 * 4
-	buf := new(bytes.Buffer)
-
-	// Escape: 32-bit BE integer. Zero.
-	err = binary.Write(buf, binary.BigEndian, uint32(0))
-	if err != nil {
-		return
-	}
-
-	// Frame length: 32-bit BE integer.
-	err = binary.Write(buf, binary.BigEndian, uint32(totalLen-2*4))
-	if err != nil {
-		return
-	}
-
-	// Control type: 32-bit BE integer.
-	err = binary.Write(buf, binary.BigEndian, uint32(CONTROL_STOP))
-	if err != nil {
-		return
-	}
-
-	// Write the control frame.
-	_, err = buf.WriteTo(enc.writer)
-	if err != nil {
-		return
-	}
-
-	return enc.Flush()
+	return ControlStop.EncodeFlush(enc.writer)
 }
 
 func (enc *Encoder) Write(frame []byte) (n int, err error) {
